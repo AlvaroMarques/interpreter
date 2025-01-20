706a8206ddc6f73082041d9cf7bea4c16f9045ce
Álvaro Marques Macedo <sc.poi458@gmail.com>
2025-01-19 22:58:25 -0300

    Revert "Parser refactor"

--- scala/interpret/build.sbt
+++ scala/interpret/build.sbt
@@ -6,13 +6,8 @@ val enumeratumVersion = "1.7.5"
 
 libraryDependencies ++= Seq(
   "com.beachape" %% "enumeratum" % enumeratumVersion,
-  "org.scalatest" %% "scalatest" % "3.2.19" % "test",
-  "com.typesafe.scala-logging" %% "scala-logging" % "3.9.4",
-  "ch.qos.logback" % "logback-classic" % "1.3.5",
-  "org.slf4j" % "slf4j-api" % "2.0.0"
+  "org.scalatest" %% "scalatest" % "3.2.19" % "test"
 )
-
-unmanagedResourceDirectories in Compile += baseDirectory.value / "src" / "main" / "resources"
 
 lazy val root = (project in file("."))
   .settings(
--- scala/interpret/src/test/scala/parser/ParserTestUtils.scala
+++ scala/interpret/src/test/scala/parser/ParserTestUtils.scala
@@ -31,12 +31,12 @@ trait ParserTestUtils {
   }
 
   def testBooleanLiteral(input: String, expectedValue: Boolean): Unit = {
-    val l = Lexer(input)
+    val l = Lexer(input).next
     val p = Parser(l)
-    val program = p.parseProgram()
+    val (program: Program, errors: Seq[ParserError]) = p.parseProgram
 
     program.statements.length shouldBe 1
-    p.errors shouldBe Matchers.empty
+    errors shouldBe Matchers.empty
     program.statements.head match {
       case stmt: ExpressionStatement =>
         stmt.expression match {
@@ -51,12 +51,12 @@ trait ParserTestUtils {
   }
 
   def testIdentifier(input: String, expectedValue: String): Unit = {
-    val l = Lexer(input)
+    val l = Lexer(input).next
     val p = Parser(l)
-    val program = p.parseProgram()
+    val (program: Program, errors: Seq[ParserError]) = p.parseProgram
 
-    program.statements.length shouldBe 1
-    p.errors shouldBe Matchers.empty
+    assert(program.statements.length == 1)
+    errors shouldBe Matchers.empty
     program.statements.head match {
       case stmt: ExpressionStatement =>
         stmt.expression match {
@@ -71,12 +71,11 @@ trait ParserTestUtils {
   }
 
   def testIntegerLiteral(input: String, expectedValue: BigInt): Unit = {
-    val l = Lexer(input)
+    val l = Lexer(input).next
     val p = Parser(l)
-    val program = p.parseProgram()
+    val (program: Program, errors: Seq[ParserError]) = p.parseProgram
 
-    program.statements.length shouldBe 1
-    p.errors shouldBe Matchers.empty
+    assert(program.statements.length == 1)
     program.statements.head match {
       case stmt: ExpressionStatement =>
         stmt.expression match {
@@ -89,4 +88,5 @@ trait ParserTestUtils {
       case _ => fail("Statement is not an expression statement")
     }
   }
+
 }
