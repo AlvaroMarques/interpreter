21a96574318658c7c5beccae3841b0bcb994c4a5
alvaro.macedo <alvaro.macedo@nubank.com.br>
2025-02-07 02:18:42 -0300

    Adding null obj treatment

--- src/main/scala/evaluator/objects/NullObject.scala
+++ src/main/scala/evaluator/objects/NullObject.scala
@@ -2,8 +2,10 @@ package evaluator.objects
 
 import evaluator.{Anything, ObjectType}
 
-case class NullObject() extends Anything {
+case class NullObjectConstructor() extends Anything {
   override def objectType: ObjectType = ObjectType.Null
 
   override def inspect: String = "null"
 }
+
+object NullObject extends  NullObjectConstructor()
--- src/test/scala/evaluator/EvaluatorMatchers.scala
+++ src/test/scala/evaluator/EvaluatorMatchers.scala
@@ -18,7 +18,7 @@ trait EvaluatorMatchers {
           case Some(t) => t.objectType == objectType
           case None => false
         },
-        s"""'$input' be evaluated for type ${objectType.toString} but instead became ${value.getOrElse(NullObject()).objectType}
+        s"""'$input' be evaluated for type ${objectType.toString} but instead became ${value.getOrElse(NullObject).objectType}
            | also, the following errors were found when parsing the code: ${
           parser.errors
             .map(_.message)
@@ -40,6 +40,10 @@ trait EvaluatorMatchers {
         matches = value match {
           case Some(t: BooleanObject) => valueToCheck match {
             case valueToCheck: Boolean => t.value == valueToCheck
+            case _ => false
+          }
+          case Some(NullObject) => valueToCheck match {
+            case valueToCheck: Option[_] if valueToCheck.isEmpty => true
             case _ => false
           }
           case Some(t: IntegerObject) => valueToCheck match {
